6d71fcac6cf2bf57a2666fc2f00251d76ddc77e6
Rohit <rohitkumarajay1997@gmail.com>
2022-09-01 23:51:19 +0545

    Created Upto Filtering by price color and brand

--- E_Shop/E_Shop/views.py
+++ E_Shop/E_Shop/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import redirect,render
-
+from store_app.models import Product
 
 def Base(request):
     return render(request,'main/base.html')
+
+def Home(request):
+    product = Product.objects.filter(status='Publish')
+
+    context={
+        'product':product
+    }
+
+    return render(request, 'main/index.html',context)
+
+
+def PRODUCT(request):
+    product = Product.objects.filter(status='Publish')
+
+    context = {
+        'product': product
+    }
+    return render(request, 'main/product.html',context)
